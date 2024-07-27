3a69f27de4bc037243ae6db60b2f8c9046f8071f
2023-anik <cse2023anik@gmail.com>
2024-07-27 21:55:26 +0600

    feat: Add new functions and update object.js for improved functionality

--- week 0/Basic HTML/Basics/function.js
+++ week 0/Basic HTML/Basics/function.js
@@ -40,3 +40,41 @@ function hello(age){
     console.log('I\'m passed through argument and my age is: '+age);
 }
 callMyName('Anik', hello);
+
+//return from one funciton to another function
+function welcomeMsg(name){
+    console.log('Welcome Mr. '+name);
+    return function options(menu){
+        console.log('Do you like '+menu+' Mr. '+name);
+    }
+}
+welcomeMsg('Anik Sen')('Coffee');
+
+// Objects are passed by reference, primitives are passed by value
+//Objects are:
+//-->arrays
+//-->function
+//-->Maps and Sets
+
+/*
+primitive data types
+1. number
+2. string
+3. null
+4. symbol
+5. undefined
+*/
+
+function aFunc(a){
+    console.log('Before changing the property: '+a.one);
+    a.one='two';
+    console.log('After changing the property: '+a.one);
+}
+
+var a={
+    one: 'one'
+}
+console.log(a);
+aFunc(a);
+console.log(a);
+
