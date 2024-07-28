df0e7dcb895a5e61b812dbb7293ce676f2342950
2023-anik <cse2023anik@gmail.com>
2024-07-28 08:41:51 +0600

    Refactor practices.js for improved readability and consistency

--- week 0/Basic Javascript/practices.js
+++ week 0/Basic Javascript/practices.js
@@ -2,4 +2,4 @@
 Get User to input a number using prompt("Enter a number:"). Check if the number is a multiple of 5 or not
 */
 let number=prompt("Enter a number:");
-console.log((number%5===0)?"YES":"NO");
+console.log((number%5==0)?number+" is a multiple of 5": number+" is not a multiple of 5");
