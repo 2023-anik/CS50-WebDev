18feee2d8899a545ec834f14d1f09978cba754c0
2023-anik <cse2023anik@gmail.com>
2024-07-30 10:07:09 +0600

    feat: Enable lazy loading for images and update CSS and JavaScript files

--- Practices/Practice4/script.js
+++ Practices/Practice4/script.js
@@ -1,25 +1,4 @@
 let para=document.querySelector("p");
 // para.classList.add("new-para");
 para.classList+=" new-para";
-let button=document.createElement("button");
-button.innerText="Add new Style";
-button.setAttribute("onclick", "addNewStyle()");
-para.after(button);
-button.style.backgroundColor="red";
-button.style.color="white";
-button.style.fontWeight="bold";
-
-// Function to add styles for the new-para class
-function addNewStyle() {
-    let style = document.createElement("style");
-    style.innerHTML = `
-        .new-para {
-            color: Green;
-            background-color: #d0f79c;
-            border: 1px solid #0056b3;
-            padding: 10px;
-            border-radius: 5px;
-        }
-    `;
-    document.head.appendChild(style);
-}
+// para.classList.remove("new-para");
