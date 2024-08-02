7c6a43a0f878b555b41cf6e5135e0e495d1f9fec
2023-anik <cse2023anik@gmail.com>
2024-08-02 14:47:28 +0600

    Refactor Async and Await example 1 to use async/await syntax

--- Basic JS part-2/Async and Await/examp1/script.js
+++ Basic JS part-2/Async and Await/examp1/script.js
@@ -2,26 +2,42 @@ let myDisplayer = (some) => {
     document.querySelector("#demo").innerHTML = some;
 }
 
-let myPromise = new Promise(function(myResolve, myReject) {
-    let req = new XMLHttpRequest();
-    req.open('GET', "mycar.html");
-    req.onload = function() {
-        if (req.status === 200) {
-            myResolve(req.response);
+// let myPromise = new Promise(function(myResolve, myReject) {
+//     let req = new XMLHttpRequest();
+//     req.open('GET', "mycar.html");
+//     req.onload = function() {
+//         if (req.status === 200) {
+//             myResolve(req.response);
+//         } else {
+//             myReject("File not Found");
+//         }
+//     };
+//     req.send();
+// });
+
+// const promiseHandle = async () => {
+//     try {
+//         const data = await myPromise;
+//         myDisplayer(data);
+//     } catch (error) {
+//         myDisplayer(error);
+//     }
+// }
+
+async function getFile() {
+    let myPromise = new Promise(function(resolve) {
+      let req = new XMLHttpRequest();
+      req.open('GET', "mycar.html");
+      req.onload = function() {
+        if (req.status == 200) {
+          resolve(req.response);
         } else {
-            myReject("File not Found");
+          resolve("File not Found!");
         }
-    };
-    req.send();
-});
-
-const promiseHandle = async () => {
-    try {
-        const data = await myPromise;
-        myDisplayer(data);
-    } catch (error) {
-        myDisplayer(error);
-    }
-}
-
-promiseHandle();
+      };
+      req.send();
+    });
+    document.getElementById("demo").innerHTML = await myPromise;
+  }
+  
+  getFile();
